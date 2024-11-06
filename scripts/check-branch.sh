391867c5ee4d89c8c22f4edbe2b3eaaa298e4446
jackytungArticulateAI <jacky.tung@articulateai.com>
2024-11-06 23:13:10 +0800

    check branch

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -2,7 +2,7 @@
 
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
-valid_prefixes=("feature/" "bugfix/" "hotfix/" "release/")
+valid_prefixes=("feature" "fix" "hotfix" "release" "prerelease")
 
 for prefix in "${valid_prefixes[@]}"; do
     if [[ $branch_name == $prefix* ]]; then
@@ -11,5 +11,11 @@ for prefix in "${valid_prefixes[@]}"; do
 done
 
 # 使用 ANSI escape codes 來設置紅色文本
-echo "\033[31mWarning: Branch name must start with one of the following prefixes:\033[0m ${valid_prefixes[*]}"
-exit 0
+echo "Warning: Branch name must start with one of the following prefixes: ${valid_prefixes[*]}"
+echo "Descriptions:"
+echo "feature: is for adding, refactoring or removing a feature"
+echo "fix: is for fixing a bug"
+echo "hotfix: is for changing code with a temporary solution and/or without following the usual process (usually because of an emergency)"
+echo "prerelease: is for releasing a prerelease version"
+echo "release: is for releasing a new version"
+exit 1
