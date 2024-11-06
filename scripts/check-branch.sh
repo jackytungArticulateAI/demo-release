3e18244cee1faea5b81e1c5b98adf185a1c8e7a2
jackytungArticulateAI <jacky.tung@articulateai.com>
2024-11-06 23:02:23 +0800

    update husky

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# 獲取當前分支名稱
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
-# 定義允許的前綴
 valid_prefixes=("feature/" "bugfix/" "hotfix/" "release/")
 
-# 檢查分支名稱是否符合規範
 for prefix in "${valid_prefixes[@]}"; do
     if [[ $branch_name == $prefix* ]]; then
         exit 0
     fi
 done
 
-echo "Warning: Branch name must start with one of the following prefixes: ${valid_prefixes[*]}"
+# 使用 ANSI escape codes 來設置紅色文本
+echo -e "\033[31mWarning: Branch name must start with one of the following prefixes:\033[0m ${valid_prefixes[*]}"
 exit 0
